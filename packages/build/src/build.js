8b4ec047e195d9205d6d411b5fa6b88187849017
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-05-20 14:17:33 +0200

    feature: update json worker url for build (#18)

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -1,4 +1,8 @@
-import { packageExtension, bundleJs } from '@lvce-editor/package-extension'
+import {
+  packageExtension,
+  bundleJs,
+  replace,
+} from '@lvce-editor/package-extension'
 import fs, { readFileSync } from 'fs'
 import path, { join } from 'path'
 import { root } from './root.js'
@@ -11,7 +15,7 @@ fs.rmSync(join(root, 'dist'), { recursive: true, force: true })
 fs.mkdirSync(path.join(root, 'dist'))
 
 const packageJson = JSON.parse(
-  readFileSync(join(extension, 'package.json')).toString()
+  readFileSync(join(extension, 'package.json')).toString(),
 )
 delete packageJson.xo
 delete packageJson.jest
@@ -20,13 +24,13 @@ delete packageJson.devDependencies
 
 fs.writeFileSync(
   join(root, 'dist', 'package.json'),
-  JSON.stringify(packageJson, null, 2) + '\n'
+  JSON.stringify(packageJson, null, 2) + '\n',
 )
 fs.copyFileSync(join(root, 'README.md'), join(root, 'dist', 'README.md'))
 fs.copyFileSync(join(extension, 'icon.png'), join(root, 'dist', 'icon.png'))
 fs.copyFileSync(
   join(extension, 'extension.json'),
-  join(root, 'dist', 'extension.json')
+  join(root, 'dist', 'extension.json'),
 )
 fs.cpSync(join(extension, 'src'), join(root, 'dist', 'src'), {
   recursive: true,
@@ -35,9 +39,23 @@ fs.cpSync(join(jsonWorker, 'src'), join(root, 'dist', 'json-worker', 'src'), {
   recursive: true,
 })
 
+const workerUrlFilePath = path.join(
+  root,
+  'dist',
+  'src',
+  'parts',
+  'JsonWorkerUrl',
+  'JsonWorkerUrl.js',
+)
+await replace({
+  path: workerUrlFilePath,
+  occurrence: 'src/jsonWorkerMain.ts',
+  replacement: 'dist/jsonWorkerMain.js',
+})
+
 await bundleJs(
   join(root, 'dist', 'json-worker', 'src', 'jsonWorkerMain.ts'),
-  join(root, 'dist', 'json-worker', 'dist', 'jsonWorkerMain.js')
+  join(root, 'dist', 'json-worker', 'dist', 'jsonWorkerMain.js'),
 )
 
 await packageExtension({
