543b0d98c22bcdc3f6e64f3e69f12d72b052f2fd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-06-11 20:01:30 +0200

    feature: improve completions (#36)

--- packages/json-worker/src/parts/GetSchemaUri/GetSchemaUri.ts
+++ packages/json-worker/src/parts/GetSchemaUri/GetSchemaUri.ts
@@ -1,5 +1,9 @@
 export const getSchemaUri = async (uri: string) => {
-  if (uri.endsWith('package.json') || uri.endsWith('test.json')) {
+  if (
+    uri.endsWith('package.json') ||
+    uri.endsWith('test.json') ||
+    uri.endsWith('index.json')
+  ) {
     return 'src/package.schema.json'
   }
   return ''
