7bf6308f18c12c382a013b404e7c1149f37c17d3
Le Vivilet <levivlet@pm.me>
2024-06-11 07:16:54 +0200

    rename fn

--- packages/json-worker/src/parts/Jsonc/Jsonc.ts
+++ packages/json-worker/src/parts/Jsonc/Jsonc.ts
@@ -1,5 +1,5 @@
 import * as Parser from '../Parser/Parser.ts'
-import * as Scanner from '../Scanner/Scanner.ts'
+import * as Scanner from '../CreateScanner/CreateScanner.ts'
 
 export const parse = (text) => {
   const scanner = Scanner.createScanner(text)
