2a2f73584ddae73fe61c99487333e1c2d0f1501c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-06-11 19:52:19 +0200

    feature: don't use quote for completion label (#35)

--- packages/json-worker/src/parts/EnumToCompletionOption/EnumToCompletionOption.ts
+++ packages/json-worker/src/parts/EnumToCompletionOption/EnumToCompletionOption.ts
@@ -4,6 +4,6 @@ import * as CompletionType from '../CompletionType/CompletionType.ts'
 export const enumToCompletionOption = (value: string): CompletionItem => {
   return {
     kind: CompletionType.Value,
-    label: `"${value}"`,
+    label: value,
   }
 }
