6f5cc584f52b68be37a88d60e020e804be8c8e17
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-06-12 09:49:17 +0200

    feature: update test file schema path (#40)

--- packages/json-worker/src/parts/GetSchemaUri/GetSchemaUri.ts
+++ packages/json-worker/src/parts/GetSchemaUri/GetSchemaUri.ts
@@ -2,7 +2,8 @@ export const getSchemaUri = async (uri: string) => {
   if (
     uri.endsWith('package.json') ||
     uri.endsWith('test.json') ||
-    uri.endsWith('index.json')
+    uri.endsWith('index.json') ||
+    uri.endsWith('file.json')
   ) {
     return 'src/package.schema.json'
   }
