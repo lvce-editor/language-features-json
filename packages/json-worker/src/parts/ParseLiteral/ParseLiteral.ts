2cbdf82cc0ea3fc82ddea23b563ab522b0ea07ca
Le Vivilet <levivlet@pm.me>
2024-08-15 21:27:30 +0200

    fix: offset for literals

--- packages/json-worker/src/parts/ParseLiteral/ParseLiteral.ts
+++ packages/json-worker/src/parts/ParseLiteral/ParseLiteral.ts
@@ -4,13 +4,14 @@ import type { Scanner } from '../Scanner/Scanner.ts'
 import * as TokenType from '../TokenType/TokenType.ts'
 
 export const parseLiteral = (scanner: Scanner): readonly AstNode[] => {
+  const offset = scanner.getOffset()
   const rawValue = scanner.scanLiteral()
   switch (rawValue) {
     case Literal.True:
       return [
         {
           type: TokenType.Boolean,
-          offset: 0,
+          offset,
           length: rawValue.length,
           childCount: 0,
         },
@@ -19,7 +20,7 @@ export const parseLiteral = (scanner: Scanner): readonly AstNode[] => {
       return [
         {
           type: TokenType.Boolean,
-          offset: 0,
+          offset,
           length: rawValue.length,
           childCount: 0,
         },
@@ -28,7 +29,7 @@ export const parseLiteral = (scanner: Scanner): readonly AstNode[] => {
       return [
         {
           type: TokenType.Null,
-          offset: 0,
+          offset,
           length: rawValue.length,
           childCount: 0,
         },
