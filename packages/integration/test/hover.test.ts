f4384a8e137550372eb432e94a0dc947174b48d5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-08-17 10:15:36 +0200

    feature: add another hover test (#58)

--- packages/integration/test/hover.test.ts
+++ packages/integration/test/hover.test.ts
@@ -69,3 +69,57 @@ test('with definitions', async () => {
       'Instructs the TypeScript compiler how to compile .ts files.',
   })
 })
+
+test.skip('with definitions - nested property', async () => {
+  const execMap = {
+    'Json.loadSchema'() {
+      return {
+        allOf: [
+          {
+            $ref: '#/definitions/compilerOptionsDefinition',
+          },
+        ],
+        definitions: {
+          compilerOptionsDefinition: {
+            properties: {
+              compilerOptions: {
+                type: 'object',
+                description:
+                  'Instructs the TypeScript compiler how to compile .ts files.',
+                properties: {
+                  composite: {
+                    description:
+                      'Enable constraints that allow a TypeScript project to be used with project references.',
+                    type: 'boolean',
+                    default: true,
+                    markdownDescription:
+                      'Enable constraints that allow a TypeScript project to be used with project references.\n\nSee more: https://www.typescriptlang.org/tsconfig#composite',
+                  },
+                },
+              },
+            },
+          },
+        },
+        type: 'object',
+      }
+    },
+  }
+  const worker = await testWorker({
+    execMap,
+  })
+  const offset = 34
+  const textDocument = {
+    uri: 'test://tsconfig.json',
+    text: `{
+  "compilerOptions": {
+    "composite": true
+  }
+}
+`,
+  }
+  expect(await worker.execute('Hover.getHover', textDocument, offset)).toEqual({
+    displayString: '',
+    documentation:
+      'Instructs the TypeScript compiler how to compile .ts files.',
+  })
+})
